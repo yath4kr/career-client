0c724e5f2262fdc45974428b21014d4237d51680
Yath4kr <yath4kr@gmail.com>
2024-06-10 11:04:49 +0530

    Cleaning Junks and Setting Up Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import Home from "./Pages/Home";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
