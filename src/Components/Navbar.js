8b120c2477fb32154e29dd55856b0abe55972172
Yath4kr <yath4kr@gmail.com>
2024-06-10 12:28:03 +0530

    Links linked with the nav buttons

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,33 @@
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
       <ul className="navbar-elements-list">
-        <li>Sign In</li>
-        <li>About</li>
-        <li>Blog</li>
-        <li>Instructors</li>
-        <li>Home</li>
+        <li>
+          <NavLink to="/signIn" className="nav-links">
+            Sign In
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className="nav-links">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/blogs" className="nav-links">
+            Blog
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/instructors" className="nav-links">
+            Instructors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/" className="nav-links">
+            Home
+          </NavLink>
+        </li>
       </ul>
     </>
   );
