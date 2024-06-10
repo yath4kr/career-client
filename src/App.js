5c0f0364abad7438b84148012fa6b02b2856ddd2
Yath4kr <yath4kr@gmail.com>
2024-06-10 12:13:12 +0530

    Navbar basic styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Navbar from "./Components/Navbar";
 import Home from "./Pages/Home";
 import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <>
+      <ul className="navbar-elements-list">
+        <li>Sign In</li>
+        <li>About</li>
+        <li>Blog</li>
+        <li>Instructors</li>
+        <li>Home</li>
+      </ul>
+    </>
+  );
+};
+export default Navbar;
